00d6c6f94fb659ab1ef9ea604e2591ee30ba846f
Rico56-web <eric.clavreul@gmail.com>
2026-03-23 23:40:48 +0100

    Update app_voile.py

--- app_voile.py
+++ app_voile.py
@@ -236,10 +236,10 @@ if st.session_state.page == "CONTACTS":
                 else:
                 c1, c2 = st.columns([1, 4])
          # Aligne bien les boutons sous le "c1, c2"
-            if c1.button("✏️", key=f"ed_{i}"):
+                    if c1.button("✏️", key=f"ed_{i}"):
                     st.session_state.edit_idx = i
                     st.rerun()
-            if c2.button("🗑️ SUPPRIMER", key=f"del_{i}", use_container_width=True):
+                    if c2.button("🗑️ SUPPRIMER", key=f"del_{i}", use_container_width=True):
                     st.session_state.contact_confirm_del = i
                     st.rerun() 
                     
