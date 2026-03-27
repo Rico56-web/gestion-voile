0884bba8c4758b55671afe892b2b5540be9349b1
Rico56-web <eric.clavreul@gmail.com>
2026-03-27 11:27:54 +0100

    Update app_voile.py

--- app_voile.py
+++ app_voile.py
@@ -281,9 +281,8 @@ if st.session_state.page == "CONTACTS":
             if b2.button("❌ NON", key=f"no_{confirm_key}", use_container_width=True):
                 st.session_state[confirm_key] = False
                 st.rerun()
-
-
-    elif st.session_state.page == "PLANNING":
+                
+        elif st.session_state.page == "PLANNING":
         st.subheader("🗓️ Planning Vesta 2026")
         
         # 1. RÉCUPÉRATION DE LA DATE DU JOUR
