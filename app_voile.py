6208dea062d86b012b6d79da910d9151b7e459be
Rico56-web <eric.clavreul@gmail.com>
2026-03-31 14:42:17 +0200

    Update app_voile.py

--- app_voile.py
+++ app_voile.py
@@ -196,7 +196,7 @@ for i, r in df_disp.iterrows():
     
     st.markdown(card_html, unsafe_allow_html=True)
 
-      # Actions Modifier / Supprimer
+    # Actions Modifier / Supprimer
       c_ed, c_del = st.columns(2)
       if c_ed.button(f"✏️ Modifier", key=f"ed_{i}", use_container_width=True):
         st.session_state.edit_idx = i
