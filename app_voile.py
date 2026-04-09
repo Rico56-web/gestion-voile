2137570ff2ac13017c88051b4c218479224e44e3
Rico56-web <eric.clavreul@gmail.com>
2026-04-09 17:05:09 +0200

    Update app_voile.py

--- app_voile.py
+++ app_voile.py
@@ -387,7 +387,20 @@ if st.session_state.page == "PLANNING":
             except: continue
 
     # --- RENDU CALENDRIER HTML ---
-    st.markdown("""<style>.full-width-cal { width: 100%; border-collapse: collapse; table-layout: fixed; } .full-width-cal td { border: 0.5px solid #eee; padding: 5px 0; }</style>""", unsafe_allow_html=True)
+    css_calendar = """
+    <style>
+        .full-width-cal { 
+            width: 100%; 
+            border-collapse: collapse; 
+            table-layout: fixed; 
+        } 
+        .full-width-cal td { 
+        border: 0.5px solid #eeeeee; 
+        padding: 5px 0; 
+    }
+</style>
+"""
+st.markdown(css_calendar, unsafe_allow_html=True)
     
     h_cal = '<table class="full-width-cal" style="text-align:center; background:white;">'
     h_cal += '<tr style="background:#f1f3f5; font-size:10px; font-weight:bold;"><td>Lu</td><td>Ma</td><td>Me</td><td>Je</td><td>Ve</td><td style="color:#d9534f;">Sa</td><td style="color:#d9534f;">Di</td></tr>'
