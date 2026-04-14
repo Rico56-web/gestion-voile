e0369f7d5127d1de62614c188729c1f0198ec638
Rico56-web <eric.clavreul@gmail.com>
2026-04-14 18:19:38 +0200

    Update app_voile.py

--- app_voile.py
+++ app_voile.py
@@ -185,6 +185,9 @@ if st.session_state.page == "CONTACTS":
             mask_s = df_c['Nom'].astype(str).str.upper().str.contains(search) | df_c['Prénom'].astype(str).str.upper().str.contains(search)
             df_c = df_c[mask_s]
         
+        # TRI CHRONOLOGIQUE INVERSE (Le plus récent/futur en haut)
+        # On utilise 'ascending=False' pour que 2027 soit avant 2026, 
+        # et que Décembre soit avant Janvier.
         df_c = df_c.sort_values(by='dt_sort', ascending=False)
 
         # Onglets
